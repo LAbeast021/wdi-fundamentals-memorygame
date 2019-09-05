9c34447c18d4d83bc42109a2e7de7b3990c4338d
Kiarash Hesampour <labeast021@Kiarashs-MacBook-Pro.local>
2019-09-05 16:23:02 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,53 @@
-var cards =[ "queen" , "queen" , "king" , "king" ];
+var cards =[ 
+    {
+        rank:"queen",
+        suit:"hearts",
+        image:"images/queen-of-hearts.png"
+
+    },
+    {
+        rank:"queen",
+        suit:"Dimonds",
+        image:"images/queen-of-diamonds.png"
+
+    },
+    {
+    rank:"king",
+    suit:"hearts",
+    image:"images/king-of-hearts.png"
+
+    },
+    {
+        rank:"king",
+        suit:"Dimonds",
+        image:"images/king-of-diamonds.png"
+
+    }
+];
 var cardsInPlay = []
 
 function checkForMatch(){
     if (cardsInPlay[0] === cardsInPlay[1]) {
         console.log("You found a match!");
+        alert("You found a match!");
       } else {
         console.log("Sorry, try again.");
+        alert("Sorry, try again.");
       }
 }
 
 function flipCard(cardId){
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId])
+    console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank)
+    console.log("user chose " + cards[cardId].suit);
+    console.log(cards[cardId].image);
  
      if( cardsInPlay.length === 2){
          checkForMatch();
    
     } 
 }
+
 
 flipCard(0);
 flipCard(2);
